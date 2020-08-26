324666613bacfdb9756c1aca97fd37448b800ed8
xj-134 <906043695@qq.com>
2020-08-26 19:11:33 +0800

    添加Disk缓存代码

---
+++ think-core/other.md
@@ -0,0 +1,4 @@
+# 将秘钥转化成psck12标准 
+keytool -importkeystore -srckeystore 目录\debug.keystore -destkeystore 目录\.keystore -deststoretype pkcs12
+
+ 
--- think-core/src/main/java/com/think/core/util/SharedPreferencesHelper.java
+++ think-core/src/main/java/com/think/core/util/SharedPreferencesHelper.java
@@ -3,6 +3,9 @@ package com.think.core.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SharedPreferencesHelper {
 
@@ -15,17 +18,26 @@ public class SharedPreferencesHelper {
 
     private static SharedPreferencesHelper instance;
 
-    /**
-     * 上下文：单例模式下使用Application 上下文
-     */
-    private SharedPreferencesHelper(Context context){
+
+    private SharedPreferencesHelper(Context context) {
         mSharedPreferences = context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
     }
+//
+//    public static SharedPreferencesHelper getInstance() {
+//        synchronized (SharedPreferencesHelper.class) {
+//            if (instance == null) {
+//                synchronized (SharedPreferencesHelper.class) {
+//                    instance = new SharedPreferencesHelper(Global.mAppContext);
+//                }
+//            }
+//        }
+//        return instance;
+//    }
 
-    public static SharedPreferencesHelper getInstance(Context context){
-        synchronized (SharedPreferencesHelper.class){
-            if(instance == null){
-                synchronized (SharedPreferencesHelper.class){
+    public static SharedPreferencesHelper getInstance(Context context) {
+        synchronized (SharedPreferencesHelper.class) {
+            if (instance == null) {
+                synchronized (SharedPreferencesHelper.class) {
                     instance = new SharedPreferencesHelper(context.getApplicationContext());
                 }
             }
@@ -33,27 +45,52 @@ public class SharedPreferencesHelper {
         return instance;
     }
 
-    public boolean contains(String key){
+    public boolean contains(String key) {
         return mSharedPreferences.contains(key);
     }
 
-    public void saveString(String key, String value){
+    public void saveString(String key, String value) {
         SharedPreferences.Editor edit = mSharedPreferences.edit();
-        edit.putString(key,value);
+        edit.putString(key, value);
         edit.apply();
     }
 
-    public String loadString(String key, String defaultValue){
-        return mSharedPreferences.getString(key,defaultValue);
+    public String loadString(String key, String defaultValue) {
+        return mSharedPreferences.getString(key, defaultValue);
     }
 
-    public void saveBoolean(String key, boolean value){
+    public void saveStringArray(String key, Set<String> values) {
         SharedPreferences.Editor edit = mSharedPreferences.edit();
-        edit.putBoolean(key,value);
+        edit.putStringSet(key, values);
         edit.apply();
     }
 
-    public boolean loadBoolean(String key,boolean defaultValue){
-        return mSharedPreferences.getBoolean(key,defaultValue);
+    public void saveStringArray(String key, String... values) {
+        Set<String> stringSet = mSharedPreferences.getStringSet(key, new HashSet<String>());
+        stringSet.addAll(Arrays.asList(values));
+        SharedPreferences.Editor edit = mSharedPreferences.edit();
+        edit.putStringSet(key, stringSet);
+        edit.apply();
+    }
+
+    public Set<String> loadStringArray(String key) {
+        return mSharedPreferences.getStringSet(key, new HashSet<String>());
+    }
+
+    public void saveBoolean(String key, boolean value) {
+        SharedPreferences.Editor edit = mSharedPreferences.edit();
+        edit.putBoolean(key, value);
+        edit.apply();
+    }
+
+    /**
+     * 获取一个boolean值
+     *
+     * @param key          key
+     * @param defaultValue 默认值
+     * @return 结果
+     */
+    public boolean loadBoolean(String key, boolean defaultValue) {
+        return mSharedPreferences.getBoolean(key, defaultValue);
     }
 }
